0562c223df5fc302e65ea1eb5b9a1244ca0d553f
Amritanshu312 <ffdarkrayon@gmail.com>
2024-10-03 09:48:05 +0530

    [iframe] fix 🩹: Iframe Attributes

--- content/watch/MainVideo/videoPlayer/VideoPlayer.jsx
+++ content/watch/MainVideo/videoPlayer/VideoPlayer.jsx
@@ -2,10 +2,18 @@ import { useWatchContext } from '@/context/Watch';
 import VideoPlayerContainer from './VideoPlayerContainer';
 
 const VideoPlayer = ({ getInstance }) => {
-  const { watchInfo } = useWatchContext()
+  const { watchInfo, MovieInfo } = useWatchContext()
   // return <VideoPlayerContainer getInstance={getInstance} />;
   return watchInfo?.iframe ?
-    <iframe src={watchInfo?.url} className="aspect-video" />
+    <iframe
+      src={watchInfo?.url}
+      className="aspect-video"
+      allowFullScreen
+      loading="lazy"
+      frameBorder="0"
+      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+      title={MovieInfo?.title || MovieInfo?.name || MovieInfo?.original_name || MovieInfo?.original_title}
+    />
     : <VideoPlayerContainer getInstance={getInstance} />;
 };
 
